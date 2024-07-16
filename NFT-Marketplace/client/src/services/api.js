0f20f216cb0ff8c2ce6a83126d78a9c8e805ec03
Donghyeon Ryu <ydh9516@gmail.com>
2024-07-16 21:51:48 +0900

    the result of npm install & truffle migration

--- NFT-Marketplace/client/src/services/api.js
+++ NFT-Marketplace/client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'http://141.223.121.131:3333/'
+  baseURL: process.env.API_URL || 'http://localhost:3333/'
 });
